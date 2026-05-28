e05f8c07fd7bff652d6f2b3e55b252c19746d54b
Mose3108082006 <monamose72@gmail.com>
2026-05-29 02:11:10 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,23 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="IMUNISCAN AI",
+    page_icon="🧠",
+    layout="wide",
+    initial_sidebar_state="collapsed"
+)
+
+# hide UI Streamlit default
+st.markdown("""
+    <style>
+        [data-testid="stSidebar"] {display: none;}
+        header {visibility: hidden;}
+        footer {visibility: hidden;}
+        #MainMenu {visibility: hidden;}
+    </style>
+""", unsafe_allow_html=True)
+
+# langsung ke dashboard
+st.switch_page("pages/dashboard.py")
+
+st.stop()
