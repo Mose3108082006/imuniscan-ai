74e52745bcd96eac2b614b94cbdc8cd2a8d7d700
Mose3108082006 <monamose72@gmail.com>
2026-05-29 07:46:43 +0700

    Update roboflow_loader.py

--- pages/ai/roboflow_loader.py
+++ pages/ai/roboflow_loader.py
@@ -1,15 +1,14 @@
-import streamlit as st
 from inference_sdk import InferenceHTTPClient
 
 def load_model():
-    # Mengambil data dari Streamlit Secrets
-    api_key = st.secrets["kdVsjgaJONiqFL0vcIB6"]
-    verifikasi = st.secrets["baby-detector-zj9py/4"]
-    # Tanda petik ganda di akhir sudah dihapus agar tidak error
-    prediksi = st.secrets["prediksi-usia/2"] 
+
+    api_key = open("models/api_key.txt").read().strip()
+
+    verifikasi = open("models/verifikasi.txt").read().strip()
+    prediksi = open("models/prediksi.txt").read().strip()
 
     CLIENT = InferenceHTTPClient(
-        api_url="https://detect.roboflow.com", 
+        api_url="https://serverless.roboflow.com",
         api_key=api_key
     )
 
