02d409867bc81574f91c17b705dc97c7106df48e
Mose3108082006 <monamose72@gmail.com>
2026-05-29 07:32:02 +0700

    Update roboflow_loader.py

--- pages/ai/roboflow_loader.py
+++ pages/ai/roboflow_loader.py
@@ -5,10 +5,11 @@ def load_model():
     # Mengambil data dari Streamlit Secrets
     api_key = st.secrets["kdVsjgaJONiqFL0vcIB6"]
     verifikasi = st.secrets["baby-detector-zj9py/4"]
-    prediksi = st.secrets["prediksi-usia/2""]
+    # Tanda petik ganda di akhir sudah dihapus agar tidak error
+    prediksi = st.secrets["prediksi-usia/2"] 
 
     CLIENT = InferenceHTTPClient(
-        api_url="https://detect.roboflow.com", # Gunakan endpoint ini untuk inference
+        api_url="https://detect.roboflow.com", 
         api_key=api_key
     )
 
